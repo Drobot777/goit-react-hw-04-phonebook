558f3079ce7230ecbf85f6eafe9ec1319828f75c
Drobot777 <121822078+Drobot777@users.noreply.github.com>
2023-08-06 22:04:23 +0300

    Done homework-04-phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,50 @@
+import {Phonebook} from './Phonebook';
+import {nanoid} from 'nanoid';
+import {useState} from 'react';
+import {Filter} from './Filter';
+import {Contacts} from './Contacts';
 export const App = () => {
+  const [contacts, setContacts] = useState ([]);
+  const [filter, setFilter] = useState ('');
+
+  const toggle = (name, tel) => {
+    let masName = [];
+    contacts.forEach (date => {
+      return masName.push (date.name);
+    });
+    if (masName.includes (name)) {
+      return alert ('Rosie Simpson is already in contacts');
+    }
+    return setContacts ([
+      ...contacts,
+      {
+        name: name,
+        id: nanoid (),
+        tel: tel,
+      },
+    ]);
+  };
+
+  const changeFilter = value => {
+    setFilter (value);
+  };
+  const deleteContact = id => {
+    let objDel = contacts.filter (el => el.id !== id);
+    setContacts (objDel);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+
+      <h1 style={{textAlign: 'center'}}>Phonebook</h1>
+      <Phonebook toggle={toggle} />
+      <h2 style={{textAlign: 'center'}}>Contact</h2>
+      <Filter changeFilter={changeFilter} />
+      <Contacts
+        allContacts={contacts}
+        valueFilter={filter}
+        deleteContact={deleteContact}
+      />
     </div>
   );
 };
